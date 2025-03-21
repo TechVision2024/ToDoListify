8b8e9ad126690f88f57ddb2db04cc18d3ca4c6f3
Mohaned Mohammed Sherhan <shrhanmhnd15@gmail.com>
2025-03-21 11:55:43 +0300

    Added user module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from './config/typeorm.config';
 import { envConfig } from './config/env.config';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(envConfig),
     TypeOrmModule.forRootAsync(typeormConfig),
-    AuthModule
+    AuthModule,
+    UserModule
   ],
   controllers: [],
   providers: [],
