a0f342719586020601a2836740ece06ec1b3bd42
Mohaned Mohammed Sherhan <shrhanmhnd15@gmail.com>
2025-03-22 14:21:42 +0300

    Added main.ts config, and added the cookies parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import * as cookiesParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
+  app.use(cookiesParser());
+  const configService: ConfigService = app.get(ConfigService);
+  const PORT: number = Number.parseInt(configService.get<string>("TODOLISTIFY_PORT"))
+  const logger: Logger = new Logger('bootstrap', {timestamp: true});
+  logger.log(`App running on ${PORT}`);
+  await app.listen(PORT);
 }
 bootstrap();
